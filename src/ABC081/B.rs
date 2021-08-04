d62f7b178fdf9b84f7d352840b908cccc0ac11fc
Rena <choco.rena0609@gmail.com>
2021-08-04 22:00:41 +0900

    ABC081 B

--- src/ABC081/B.rs
+++ src/ABC081/B.rs
@@ -1,28 +1,33 @@
 use proconio::input;
 use proconio::source::auto::AutoSource;
+
 fn main() {
     // デバッグ方法
-    let source = AutoSource::from("3
-    8 12 40");
+    // let source = AutoSource::from(
+    //     "6
+    //     382253568 723152896 37802240 379425024 404894720 471526144
+    //     ",
+    // );
     input! {
-        from source,
-        n: i32,
-        a: [i32; n]
+        // from source,
+        n: usize,
+        mut a: [usize; n]
     };
-    println!("{}, {:?}", n, a);
-    // let mut flg = true;
-    // let ans = 0;
+    let mut flg = true;
+    let mut ans = 0;
 
-    // for i in 0..n {
-    //     if a[i] % 2 != 0 {
-    //         flg = false;
-    //     } else {
-    //         a[i] >= 1;
-    //     }
-    // }
-    // if flg == true {
-    //     ans += 1;
-    // }
-
-    // println!();
+    loop {
+        for i in 0..n {
+            if a[i] % 2 == 1 {
+                flg = false;
+            }
+            a[i] /= 2;
+        }
+        if flg == true {
+            ans += 1;
+        } else {
+            println!("{}", ans);
+            break;
+        }
+    }
 }
