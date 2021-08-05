dc6a88227fc5834dc6b42238242a671fb0859210
Rena <choco.rena0609@gmail.com>
2021-08-05 21:02:01 +0900

    ABC083 B

--- src/ABC083/B.rs
+++ src/ABC083/B.rs
@@ -1,20 +1,27 @@
-use prcn_lib::math::dight_sum;
 use proconio::input;
-use proconio::source::auto::AutoSource;
+// use proconio::source::auto::AutoSource;
+
+// 各桁の和
+fn sum(i: u32) -> u32 {
+    i.to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .sum::<u32>()
+}
 
 fn main() {
-    // let source = AutoSource::from("10 1 2");
+    // let source = AutoSource::from("100 4 16");
 
     input! {
         // from source,
-        n: u64,
-        a: u64,
-        b: u64
+        n: u32,
+        a: u32,
+        b: u32
     };
 
     let mut ans = 0;
     for i in 0..n + 1 {
-        if a <= dight_sum(i) && b >= dight_sum(i) {
+        if a <= sum(i) && b >= sum(i) {
             ans += i;
         }
     }
