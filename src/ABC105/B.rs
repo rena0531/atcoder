83db10b03dffd2e9f46b44c5ae1cc4089f45e267
Rena <choco.rena0609@gmail.com>
2021-08-08 19:02:47 +0900

    105 B

--- src/ABC105/B.rs
+++ src/ABC105/B.rs
@@ -1,23 +1,19 @@
 use proconio::input;
 
 fn main() {
-    // 数字が大きすぎる場合使えないため、Stringで入力を受け取る
-    // input! {
-    //     n: u64
-    // };
     input! {
-        n: String,
+        n: i32
     };
-    let n: Vec<u32> = n.chars().map(|x| x.to_digit(10).unwrap()).collect();
 
-    let n_len = n.len();
-
-    let mut sum = 0;
-    for i in 0..n_len {
-        sum += n[i];
+    let mut cnt = 0;
+    for i in 0..=n {
+        for j in 0..=n {
+            if 4 * i + 7 * j == n {
+                cnt += 1;
+            }
+        }
     }
-
-    if sum % 9 == 0 || sum == 0 {
+    if cnt > 0 {
         println!("Yes");
     } else {
         println!("No");
